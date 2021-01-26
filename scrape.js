9f1d1d95b17a6feb79e5d509b16c6c1ea39dd5a3
Roberto Valdez <robertov66@yahoo.com>
2021-01-25 21:48:36 -0500

    first commit

---
+++ scrape.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.listen(port);
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+const axios = require('axios');
+const { parse } = require('node-html-parser');
+
+(async () => {
+    const page = await axios.get('https://www.worldometers.info/coronavirus/country/us/');
+    const html = page.data;
+    const dom = parse(html);
+    const covidCaseCounter = dom.querySelector('.maincounter-number').text;
+    console.log(covidCaseCounter);
+
+    app.get('', (req, res) => {
+        res.render('index', { text: 'Total COVID-19 Cases: ' + covidCaseCounter});
+    });
+
+    app.get('/tips', (req, res) => {
+        res.render('tips');
+    });
+})();
